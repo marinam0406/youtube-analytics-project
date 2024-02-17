4bda12b91cb6517357a245a37d561f576c4e0ac4
marinam0406 <marinam0406@yandex.ru>
2024-02-17 22:06:35 +0300

    add video.py

--- src/video.py
+++ src/video.py
@@ -16,15 +16,21 @@ class Video:
         - количество лайков
         """
         self.video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
-        self.url = f"https://www.youtube.com/channel/{self.video_id}"
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
+            self.url = f"https://www.youtube.com/channel/{self.video_id}"
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except Exception:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         """Возвращает заголовок"""
-        return f"{self.video_title}"
+        return f"{self.title}"
 
 class PLVideo(Video):
     """
